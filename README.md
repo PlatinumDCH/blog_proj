5d607f3ba0f92b0eddac42de2c67f62776b6000d
dima cheban <dimacheban23@gmail.com>
2025-01-18 15:06:45 +0200

    update and create
    - create config class backend/core/config
    - update .env file (settings connection DB)

---
+++ README.md
@@ -0,0 +1,9 @@
+technology stack
+FronEnd: Jinja2, HTML, CSS, Bootstrap
+API: core FastAPI, RestAPI
+Pytest:
+Git/GitHub:
+sqlalchemy:
+alembic:
+Postgresql:
+Docker:
--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,23 @@
-class Settings:
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
     PROJECT_NAME: str = 'Project-Blog'
     PROJECT_VERSION: str = '1.0.0'
 
+    POSTGRES_USER:str = 'test'
+    POSTGRES_PASSWORD:str = 'test'
+    POSTGRES_SERVER:str = 'localhost'
+    POSTGRES_PORT:int = 5432
+    POSTGRES_DB:str = 'blog_db'
+    DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+    model_config = SettingsConfigDict(
+        extra='ignore'
+    )
+
+    class Conf:
+        env_file = '.env'
+        env_file_encoding = 'unf-8'
+
 settings = Settings()
+
