c673c4b6d06fcc1edfe244185f00fa46b7f2b71f
dima cheban <dimacheban23@gmail.com>
2025-01-23 01:46:56 +0200

    add new feature
    -get all blogs in database
    -get concrete glog (used id)
    -issued database process get_all_blogs
    -issue database prcess get blog by id

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 
 from backend.db.connection import get_connection
 from backend.schemas.blog import ShowBlog, CreateBlog
-from backend.db.repository.blog import create_new_blog
+from backend.db.repository.blog import create_new_blog, retreive_blog, list_blogs
 
 router = APIRouter()
 
@@ -12,3 +12,20 @@ router = APIRouter()
 async def create_blog(blog: CreateBlog, db: AsyncSession= Depends(get_connection)):
     new_blog = await create_new_blog(blog=blog,db=db,author_id=1)
     return new_blog
+
+@router.get('/blogs', response_model=list[ShowBlog])
+async def get_all_blogs(db: AsyncSession = Depends(get_connection)):
+    """получить все записи пользователя"""
+    blogs = await list_blogs(db)
+    return blogs
+
+@router.get('/blog/{id}', response_model=ShowBlog)
+async def get_blog(id: int, db:AsyncSession = Depends(get_connection)):
+    blog = await retreive_blog(id, db)
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Blog with id {id} does not exists'
+        )
+    return blog
+
--- backend/db/repository/blog.py
+++ backend/db/repository/blog.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession 
 from backend.schemas.blog import CreateBlog
 from backend.models.base import Blog
-
+from sqlalchemy import select
 
 async def create_new_blog(blog: CreateBlog, db: AsyncSession, author_id:int = 1):
     new_blog = Blog(
@@ -12,3 +12,15 @@ async def create_new_blog(blog: CreateBlog, db: AsyncSession, author_id:int = 1)
     await db.commit()
     await db.refresh(new_blog)
     return new_blog
+
+async def retreive_blog(id:int, db:AsyncSession):
+    result = await db.execute(select(Blog).filter(Blog.id == id))
+    blog = result.scalar_one_or_none()
+    return blog
+
+
+async def list_blogs(db:AsyncSession):
+    result = await db.execute(select(Blog).filter(Blog.is_active==True))
+    blogs = result.scalars().all()
+    return blogs
+
